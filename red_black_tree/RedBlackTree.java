198adaa372f564692e7a32b9ea67f76da6bfebdf
Abhishek <dasabhishek0790>
2020-09-26 20:05:17 +0530

    Red Black Tree in Java

--- red_black_tree/RedBlackTree.java
+++ red_black_tree/RedBlackTree.java
@@ -1,8 +1,6 @@
 import java.util.ArrayDeque;
 import java.util.Queue;
 
-import org.graalvm.compiler.graph.Node;
-
 public class RedBlackTree {
     private Node root;
     private Queue<Node> levelOrderTraversalQueue;
@@ -11,44 +9,44 @@ public class RedBlackTree {
 
     }
 
-    public void rotateLeft(Node root, Node point) {
-        Node point_right = point.right;
-        point.right = point_right.left;
-        if (point.right != null) {
-            point.right.parent = point;
+    public void rotateLeft(Node root, Node node) {
+        Node node_right = node.right;
+        node.right = node_right.left;
+        if (node.right != null) {
+            node.right.parent = node;
         }
 
-        point_right.parent = point.parent;
-        if (point.parent == null) {
-            root = point_right;
-        } else if (point == point.parent.left) {
-            point.parent.left = point_right;
+        node_right.parent = node.parent;
+        if (node.parent == null) {
+            root = node_right;
+        } else if (node == node.parent.left) {
+            node.parent.left = node_right;
         } else {
-            point.parent.right = point_right;
+            node.parent.right = node_right;
         }
 
-        point_right.left = point;
-        point.parent = point_right;
+        node_right.left = node;
+        node.parent = node_right;
     }
 
-    public void rotateRight(Node root, Node point) {
-        Node point_left = point.left;
-        point.left = point_left.right;
-        if (point.left != null) {
-            point.left.parent = point;
+    public void rotateRight(Node root, Node node) {
+        Node node_left = node.left;
+        node.left = node_left.right;
+        if (node.left != null) {
+            node.left.parent = node;
         }
 
-        point_left.parent = point.parent;
-        if (point.parent == null) {
-            root = point_left;
-        } else if (point == point.parent.left) {
-            point.parent.left = point_left;
+        node_left.parent = node.parent;
+        if (node.parent == null) {
+            root = node_left;
+        } else if (node == node.parent.left) {
+            node.parent.left = node_left;
         } else {
-            point.parent.left = point_left;
+            node.parent.left = node_left;
         }
 
-        point_left.right = point;
-        point.parent = point_left;
+        node_left.right = node;
+        node.parent = node_left;
     }
 
     public void levelOrderTraversal(final Node root) {
@@ -72,7 +70,55 @@ public class RedBlackTree {
     }
 
     public void prepareRedBlackFromBST(Node root, Node newNode) {
+        Node parent = null; 
+        Node grand_parent = null;
+        while ((!newNode.equals(root)) && (!newNode.color.equals(RedBlackColor.BLACK)) && (newNode.parent.color.equals(RedBlackColor.RED))) {
+            parent = newNode.parent; 
+            grand_parent = newNode.parent.parent;
 
+            if (parent == grand_parent.left) {
+                Node uncle = grand_parent.right;
+                if (uncle != null && uncle.color.equals(RedBlackColor.RED)) { 
+                    grand_parent.color = RedBlackColor.RED;
+                    parent.color = RedBlackColor.BLACK; 
+                    uncle.color = RedBlackColor.BLACK; 
+                    newNode = grand_parent;
+                } else {
+                    if (newNode == parent.right) { 
+                        this.rotateLeft(root, parent); 
+                        newNode = parent; 
+                        parent = newNode.parent; 
+                    }
+                    this.rotateRight(root, grand_parent); 
+                    swapColors(parent.color, grand_parent.color); 
+                    newNode = parent; 
+                } 
+            } else {
+                Node uncle = grand_parent.left; 
+                if ((uncle != null) && (uncle.color.equals(RedBlackColor.RED))) { 
+                    grand_parent.color = RedBlackColor.RED;
+                    parent.color = RedBlackColor.BLACK; 
+                    uncle.color = RedBlackColor.BLACK; 
+                    newNode = grand_parent; 
+                } else {
+                    if (newNode == parent.left) { 
+                        this.rotateRight(root, parent); 
+                        newNode = parent; 
+                        parent = newNode.parent; 
+                    }
+                    rotateLeft(root, grand_parent); 
+                    swapColors(parent.color, grand_parent.color); 
+                    newNode = parent; 
+                }  
+            }
+        }
+        root.color = RedBlackColor.BLACK;
+    }
+
+    private void swapColors(RedBlackColor c1, RedBlackColor c2) {
+        RedBlackColor temp = c1;
+        c1 = c2;
+        c2 = temp;
     }
 
     public void insertNode(int newData) {
